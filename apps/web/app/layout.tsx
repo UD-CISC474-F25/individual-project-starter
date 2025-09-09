938fbbee9f88db7e81248641f95566bbe2d10f9a
Hairum Qureshi <hqureshi@udel.edu>
2025-09-09 17:35:30 -0400

    re-added postcss config

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
         {children}
       </body>
     </html>
