ed0595a667c33d73193687c317032c79cfbe6e35
Hairum Qureshi <hqureshi@udel.edu>
2025-09-10 10:12:42 -0400

    changed text color

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,5 @@
 // port 3001
 
 export default function Home() {
-  return (
-    <h1 className="text-lg font-semibold text-green-500">Hello World!!</h1>
-  );
+  return <h1 className="text-lg font-semibold text-sky-500">Hello World!!</h1>;
 }
