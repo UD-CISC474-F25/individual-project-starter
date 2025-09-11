987465313e424e6f67e71980258299456a4e307a
Hairum Qureshi <hqureshi@udel.edu>
2025-09-11 00:47:20 -0400

    added hover effect to Course component

--- apps/web/components/Course.tsx
+++ apps/web/components/Course.tsx
@@ -1,6 +1,6 @@
 export default function Course({ courseName }: { courseName?: string }) {
   return (
-    <div className="w-11/12 p-4 border border-slate-500 bg-slate-950 text-white m-auto my-5 rounded-sm">
+    <div className="w-11/12 p-4 border hover:cursor-pointer border-slate-500 bg-slate-950 text-white m-auto my-5 rounded-sm">
       {courseName}
     </div>
   );
