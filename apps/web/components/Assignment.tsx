116f165b1d8aba36cea793010f86ed84b724bef0
Hairum Qureshi <hqureshi@udel.edu>
2025-09-11 16:51:32 -0400

    added border and changed background color

--- apps/web/components/Assignment.tsx
+++ apps/web/components/Assignment.tsx
@@ -2,22 +2,19 @@ export default function Assignment({
   assignmentTitle,
   score,
   outOf,
-  date,
 }: {
   assignmentTitle: string;
   score: number;
   outOf: number;
-  date: string;
 }) {
   return (
-    <div className="flex mb-4 p-4 bg-slate-700 rounded-md w-full text-white">
+    <div className="flex mb-4 p-3 border-2 border-slate-500 bg-slate-400 rounded-md w-full text-white">
       {assignmentTitle}
-      <div className="ml-auto flex flex-col">
+      <div className="ml-auto flex">
         <div className="text-right">
-          <span className="text-lg font-semibold text-sky-400">{score}</span>/
+          <span className="text-lg font-semibold text-sky-700">{score}</span>/
           {outOf}
         </div>
-        <span className="text-slate-400 text-sm">{date}</span>
       </div>
     </div>
   );
