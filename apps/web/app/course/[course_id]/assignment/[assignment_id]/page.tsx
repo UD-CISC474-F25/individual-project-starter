d0f676fc309815db7706a5ce11c0c34af9e9975d
Hairum Qureshi <hqureshi@udel.edu>
2025-09-10 23:00:09 -0400

    designed assignment page

--- apps/web/app/course/[course_id]/assignment/[assignment_id]/page.tsx
+++ apps/web/app/course/[course_id]/assignment/[assignment_id]/page.tsx
@@ -1,3 +1,43 @@
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="w-full p-3">
+      <div className="flex items-center">
+        <h2 className="font-semibold text-3xl mb-2">Assignment 1</h2>
+        <button className="ml-auto py-2 px-3 bg-sky-700 text-white rounded-md hover:cursor-pointer">
+          Submit Assignment
+        </button>
+      </div>
+      <div className="my-5">
+        <h3 className="text-xl font-semibold">Assignment Details</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. At quia nisi
+          unde id, dolorem recusandae iste, odio nostrum nulla repudiandae, ut
+          nesciunt sequi! Voluptatibus sapiente eveniet veniam! Quidem, nemo
+          ducimus. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Odit ullam dolore iusto! Repellat iure eos ab repellendus velit?
+          Eaque, doloremque. Voluptatum esse autem laboriosam eum possimus
+          asperiores veniam, tempore ducimus? <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+          iure dolores harum impedit inventore molestiae doloribus aliquid
+          facilis perspiciatis nostrum, quo nobis sapiente nam animi minus
+          consequuntur? Vero, voluptatum. Enim! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Porro unde, temporibus repudiandae
+          aliquid neque maiores quidem nostrum omnis fugiat debitis ea
+          reprehenderit voluptate reiciendis, possimus atque, earum sit sapiente
+          accusantium! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Consectetur numquam nam illum libero blanditiis inventore rerum
+          asperiores veniam nobis exercitationem, pariatur necessitatibus
+          quibusdam, nihil amet aperiam doloribus excepturi, debitis quo. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Sit error pariatur
+          illo dolore? Quae ullam quisquam aperiam laborum, ipsam error harum
+          vitae recusandae officia molestias maiores rerum. Eius, laborum
+          doloribus.
+        </p>
+      </div>
+      <div className="w-full">
+        <h3 className="text-xl font-semibold">Assignment Feedback</h3>
+      </div>
+    </div>
+  );
 }
