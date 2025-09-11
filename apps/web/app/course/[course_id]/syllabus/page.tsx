549044054433e3be30687ecbcf9a2a8fa656d7ba
Hairum Qureshi <hqureshi@udel.edu>
2025-09-10 23:08:15 -0400

    added 'course syllabus' route page and added placeholder content

---
+++ apps/web/app/course/[course_id]/syllabus/page.tsx
@@ -0,0 +1,38 @@
+export default function page() {
+  return (
+    <div className="w-full p-2">
+      <h2 className="font-semibold text-2xl">Course Syllabus</h2>
+      <div className="my-3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla id
+          laboriosam numquam ab soluta nesciunt, ipsum dolor officia. Odit
+          cumque perferendis voluptatem perspiciatis quia qui, laboriosam eius
+          recusandae tenetur minus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quia, temporibus molestiae? Eos, quae obcaecati.
+          Modi voluptates voluptas, hic, at vel non saepe tempore, dicta
+          mollitia nobis esse quasi recusandae consequatur. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Placeat facere incidunt debitis
+          laudantium nesciunt inventore commodi quibusdam adipisci perferendis.
+          Voluptatem odit quibusdam fugiat esse labore earum totam hic
+          reiciendis iste. Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Amet, nisi quos labore nostrum voluptatibus doloribus. Assumenda
+          laudantium et, culpa nisi incidunt dolore aspernatur, esse porro
+          aperiam vel quam molestiae nihil? <br /> <br /> Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Ex et exercitationem ab!
+          Voluptatibus ea quas culpa libero delectus neque voluptate rem eveniet
+          maiores vel, voluptas molestias officia, laudantium aut doloremque!
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis eum
+          quia et in, cumque necessitatibus ipsam nesciunt aut qui quasi atque,
+          sequi quod molestias officia perferendis consequatur placeat, iste
+          quaerat! <br /> Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Laborum distinctio quam neque consequatur placeat voluptatum
+          rerum dolorem error. Dolores dignissimos quis aliquid deserunt illo
+          incidunt. Quis quasi est accusamus deleniti! Lorem ipsum dolor sit
+          amet consectetur, adipisicing elit. Quibusdam ad, praesentium vitae ea
+          nihil eos id sit molestiae. Perspiciatis odio eligendi vitae omnis
+          sint ratione ullam illum animi commodi. Accusamus.
+        </p>
+      </div>
+    </div>
+  );
+}
