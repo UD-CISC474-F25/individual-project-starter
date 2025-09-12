69101202f623e8d1fad8755454a3260ecf410de3
Sufyan Masood <sufyanm@udel.edu>
2025-09-12 01:39:24 -0400

    created and finished login page

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, CSSProperties } from "react";
 
 interface ButtonProps {
   children: ReactNode;
   className?: string;
   appName: string;
+  type?: "button" | "submit" | "reset";
+  style?: CSSProperties;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, appName, type = "button", style, onClick }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      type={type}
+      style={style}
+      onClick={onClick}
     >
       {children}
     </button>
