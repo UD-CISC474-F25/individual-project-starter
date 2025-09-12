20d22810598209548ebdce618a45d521ab4d0cb5
Hairum Qureshi <hqureshi@udel.edu>
2025-09-12 00:57:52 -0400

    added styling to assignments page which is rendering out the modules

--- apps/web/app/course/[course_id]/assignments/page.tsx
+++ apps/web/app/course/[course_id]/assignments/page.tsx
@@ -1,3 +1,14 @@
-export default function page() {
-  return <div>page</div>;
+import Module from '../../../../components/modules/Module';
+
+// TODO - add search bar
+
+export default function Assignments() {
+  return (
+    <div className="w-full">
+      <Module moduleName={'Module 1'} numSections={10} />
+      <Module moduleName={'Module 2'} numSections={5} />
+      <Module moduleName={'Module 3'} numSections={7} />
+      <Module moduleName={'Module 4'} numSections={3} />
+    </div>
+  );
 }
