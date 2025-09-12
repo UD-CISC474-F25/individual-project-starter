35492d597f0938b4a9c1ec8dc7276e71bd6501f7
Hairum Qureshi <hqureshi@udel.edu>
2025-09-11 20:52:39 -0400

    changed Course component background colors

--- apps/web/components/Course.tsx
+++ apps/web/components/Course.tsx
@@ -1,6 +1,6 @@
 export default function Course({ courseName }: { courseName?: string }) {
   return (
-    <div className="w-11/12 p-4 border hover:cursor-pointer border-slate-500 bg-slate-950 text-white m-auto my-5 rounded-sm">
+    <div className="w-11/12 p-4 hover:cursor-pointer border-2 border-slate-400 bg-slate-700 text-white m-auto my-5 rounded-sm">
       {courseName}
     </div>
   );
