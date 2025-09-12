e687c2f20ecc772b2ceb1556a09280f39bda4d49
Hairum Qureshi <hqureshi@udel.edu>
2025-09-11 22:55:42 -0400

    added icons to each navbar links

--- apps/web/components/navbar/SideNavbar.tsx
+++ apps/web/components/navbar/SideNavbar.tsx
@@ -4,6 +4,13 @@ import Link from 'next/link';
 import Image from 'next/image';
 import defaultUserPfp from '../../public/assets/default-user-pfp.jpg';
 import { useParams } from 'next/navigation';
+import { MdOutlineSchool } from 'react-icons/md';
+import { RxDashboard } from 'react-icons/rx';
+import { MdOutlineGrade } from 'react-icons/md';
+import { PiPencilLineLight } from 'react-icons/pi';
+import { LuNewspaper } from 'react-icons/lu';
+import { MdOutlinePermMedia } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
 
 export default function SideNavbar() {
   const params = useParams();
@@ -51,36 +58,42 @@ export default function SideNavbar() {
         {course_id ? (
           <div className="flex flex-col gap-3">
             <Link href="/" className="hover:bg-sky-900 p-1 hover:rounded-sm">
+              <RxDashboard className="inline mr-2 mb-1 text-lg" />
               Dashboard
             </Link>
             <Link
               href={`/course/${course_id}`}
               className="hover:bg-sky-900 p-1 hover:rounded-sm"
             >
+              <FaSchool className="inline mr-2 mb-1 text-lg" />
               Course Page
             </Link>
             <Link
               href={`/course/${course_id}/grades`}
               className="hover:bg-sky-900 p-1 hover:rounded-sm"
             >
+              <MdOutlineGrade className="inline mr-2 mb-1 text-lg" />
               Grades
             </Link>
             <Link
               href={`/course/${course_id}/assignments`}
               className="hover:bg-sky-900 p-1 hover:rounded-sm"
             >
+              <PiPencilLineLight className="inline mr-2 mb-1 text-lg" />
               Assignments
             </Link>
             <Link
               href={`/course/${course_id}/syllabus`}
               className="hover:bg-sky-900 p-1 hover:rounded-sm"
             >
+              <LuNewspaper className="inline mr-2 mb-1 text-lg" />
               Syllabus
             </Link>
             <Link
               href={`/course/${course_id}/media`}
               className="hover:bg-sky-900 p-1 hover:rounded-sm"
             >
+              <MdOutlinePermMedia className="inline mr-2 mb-1 text-lg" />
               Media & Files
             </Link>
             <hr className="border-sky-700" />
@@ -91,6 +104,7 @@ export default function SideNavbar() {
                 href={`/course/${course.id}`}
                 className="hover:bg-sky-900 p-1 hover:rounded-sm"
               >
+                <MdOutlineSchool className="inline mr-2 mb-1 text-lg" />
                 {course.name}
               </Link>
             ))}
@@ -98,6 +112,7 @@ export default function SideNavbar() {
         ) : (
           <div className="flex flex-col gap-3">
             <Link href="/" className="hover:bg-sky-900 p-1 hover:rounded-sm">
+              <RxDashboard className="inline mr-2 mb-1 text-lg" />
               Dashboard
             </Link>
             <hr className="border-sky-700" />
@@ -108,6 +123,7 @@ export default function SideNavbar() {
                 href={`/course/${course.id}`}
                 className="hover:bg-sky-900 p-1 hover:rounded-sm"
               >
+                <MdOutlineSchool className="inline mr-2 mb-1 text-lg" />
                 {course.name}
               </Link>
             ))}
